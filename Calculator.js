028f24cd8e28764a9e53fc7694a83ac9d2db61a6
jsegal205 <Jim@Jim-Segals-MacBook.local>
2013-09-04 07:57:48 -0500

    init commit

---
+++ Calculator.js
@@ -0,0 +1,48 @@
+e='';
+ops=['+','-','*','/'];
+
+function addtoWindow(code) {
+	w=getWindow();
+	w.innerHTML=w.innerHTML+code;
+	e=e+code;
+}
+
+function addOpstoWindow(code){
+	if (isOpLastChar()){
+		w=getWindow();
+		w.innerHTML=w.innerHTML+code;
+		e=e+code;
+	}
+}
+
+function clearWindow() {
+	w=getWindow();
+	w.innerHTML = '&nbsp';
+	e='';
+}
+
+function doMath(){
+	try{
+		if(e!='' && isOpLastChar()){
+			w=getWindow();
+			w.innerHTML = eval(e);
+			e=w.innerHTML;
+		}
+	}
+	catch(err){
+		alert(err);
+	}
+}
+
+function isOpLastChar() {
+	for (var i=0;i<ops.length;i++){
+		if(w.innerHTML.substring(w.innerHTML.length-1)==ops[i]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function getWindow() {
+	return document.getElementById('window');
+}
